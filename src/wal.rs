f8cefbdbb3ac02e4d4d3fc865df5e4dc9bee10e2
Jack <56563911+jdockerty@users.noreply.github.com>
2024-08-10 12:44:09 +0100

    feat: wal segments (#14)
    * refactor: do not conflate wal append and rotation
    
    The WAL itself does not need to handle the rotation of its own file. We
    can abstract this a little higher by introducing a `WalManager` later
    on.
    
    * chore: remove unnecessary log_file_path in struct
    
    * feat: use internal buffer for wal, wrapping append_batch
    
    * chore: remove generics
    
    These were added unnecessarily too early. It is just causing more
    complication for no reason, removing for now.
    
    * feat: wip split wal to segments
    
    * chore: current_id var in rotation
    
    * feat: extend wal rotation test for segments

--- src/wal.rs
+++ src/wal.rs
@@ -2,23 +2,66 @@
 #![allow(dead_code)]
 
 use std::io::Write;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 use std::sync::atomic::Ordering;
 use std::{fs::File, sync::atomic::AtomicU64};
 
 use serde::{Deserialize, Serialize};
 
-pub const WAL_MAX_SIZE_BYTES: u64 = 1048576; // 1 MiB
+pub const WAL_MAX_SEGMENT_SIZE_BYTES: u64 = 64 * 1024 * 1024; // 64 MiB
 
 /// Wal maintains a write-ahead log (WAL) as an append-only file to provide persistence
 /// across crashes of the system.
-pub struct Wal<P: AsRef<Path>> {
-    id: AtomicU64,
-    log_file: File,
-    log_directory: P,
-    log_file_path: PathBuf,
+pub struct Wal {
+    log_directory: PathBuf,
     current_size: u64,
     max_size: u64,
+    buffer: Vec<u8>,
+
+    /// Closed, immutable WAL segment IDs.
+    ///
+    /// Once the incoming WAL that was placed into a memtable has subsequently
+    /// been flushed to an SSTable, these can be safely archived or deleted.
+    closed_segments: Vec<u64>,
+
+    /// Active segment file
+    segment: Segment,
+}
+
+struct Segment {
+    /// ID of the segment.
+    id: AtomicU64,
+    log_file: File,
+}
+
+impl Segment {
+    pub fn new(id: u64, path: PathBuf) -> Self {
+        let id = AtomicU64::new(id);
+        let log_file_path = format!(
+            "{}/{}.wal",
+            path.display(),
+            id.load(std::sync::atomic::Ordering::Acquire)
+        );
+        let new_segment = std::fs::OpenOptions::new()
+            .create(true)
+            .append(true)
+            .open(log_file_path)
+            .unwrap();
+
+        Self {
+            id,
+            log_file: new_segment,
+        }
+    }
+
+    pub fn flush(&mut self) {
+        self.log_file.sync_all().unwrap()
+    }
+
+    // The current WAL id.
+    pub fn id(&self) -> u64 {
+        self.id.load(Ordering::Acquire)
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -27,81 +70,53 @@ pub enum WalEntry {
     Delete { key: Vec<u8> },
 }
 
-impl<P: AsRef<Path>> Wal<P> {
-    pub fn new(id: u64, log_directory: P, max_size: u64) -> Self {
-        let id = AtomicU64::new(id);
-        let log_file_path = format!(
-            "{}/{}.wal",
-            log_directory.as_ref().display(),
-            id.load(std::sync::atomic::Ordering::Acquire)
-        );
-
-        let log_file = std::fs::File::options()
-            .create(true)
-            .append(true)
-            .read(true)
-            .open(&log_file_path)
-            .expect("Can create new WAL file");
-
+impl Wal {
+    pub fn new(id: u64, log_directory: PathBuf, max_size: u64) -> Self {
         Self {
-            id,
-            log_file,
-            log_file_path: PathBuf::from(log_file_path),
-            log_directory,
+            log_directory: log_directory.clone(),
             current_size: 0,
             max_size,
+            buffer: Vec::new(),
+            segment: Segment::new(id, log_directory),
+            closed_segments: Vec::new(),
         }
     }
 
-    /// Append a key-value pair to the WAL file.
-    pub fn append(&mut self, entry: WalEntry) -> u64 {
-        // +1 for the newline inclusion
-        let size = bincode::serialized_size(&entry).unwrap() + 1;
-        bincode::serialize_into(&self.log_file, &entry).unwrap();
-        writeln!(&self.log_file).unwrap();
-        self.current_size += size;
+    /// Append one or more key-value pairs to the WAL file.
+    ///
+    pub fn append(&mut self, entries: Vec<WalEntry>) -> u64 {
+        self.append_batch(entries)
+    }
 
-        if self.current_size > self.max_size {
-            eprintln!("WAL rotation");
-            // Create a new wal file
-            self.rotate()
+    /// Append a batch of [`WalEntry`] into the current log file.
+    fn append_batch(&mut self, entries: Vec<WalEntry>) -> u64 {
+        for e in entries {
+            // TODO: create append_entry func which is generic over Write trait
+            bincode::serialize_into(&mut self.buffer, &e).unwrap();
+            writeln!(&mut self.buffer).unwrap();
         }
-
-        size
+        self.segment.log_file.write_all(&self.buffer).unwrap();
+        self.current_size = self.buffer.len() as u64;
+        self.buffer.len() as u64
     }
 
-    /// Get the current path of the WAL file.
-    pub fn path(&self) -> &Path {
-        &self.log_file_path
+    /// Get the current path of the active WAL segment file.
+    pub fn path(&self) -> PathBuf {
+        format!("{}/{}.wal", self.log_directory.display(), self.segment.id()).into()
     }
 
-    /// Rotate the current WAL file.
+    /// Rotate the currently active WAL segment file.
     ///
     /// This flushes all operations to the current file before creating a new file.
     pub fn rotate(&mut self) {
-        self.log_file.flush().unwrap();
+        eprintln!("WAL rotation");
+        self.segment.flush();
 
-        let new_id = self.next_id();
-        let log_file = std::fs::File::options()
-            .create(true)
-            .append(true)
-            .read(true)
-            .open(self.log_directory.as_ref().join(format!("{new_id}.wal")))
-            .expect("Can rotate WAL file");
-
+        let current_id = self.segment.id();
+        self.closed_segments.push(current_id);
+        let new_segment = Segment::new(current_id + 1, self.log_directory.clone());
         self.current_size = 0;
-        self.log_file = log_file;
-    }
-
-    // The current WAL id.
-    pub fn id(&self) -> u64 {
-        self.id.load(Ordering::Acquire)
-    }
-
-    /// Set the next WAL id and return its value.
-    pub fn next_id(&self) -> u64 {
-        // This returns the previous, so we add one to it to get the new value.
-        self.id.fetch_add(1, Ordering::Acquire) + 1
+        self.segment = new_segment;
     }
 
     pub fn size(&self) -> u64 {
@@ -111,6 +126,8 @@ impl<P: AsRef<Path>> Wal<P> {
 
 #[cfg(test)]
 mod test {
+    use std::io::{BufRead, BufReader};
+
     use super::*;
 
     use tempdir::TempDir;
@@ -120,49 +137,63 @@ mod test {
     #[test]
     fn write_to_wal() {
         let temp_dir = TempDir::new("write_wal").unwrap();
-        let mut wal = Wal::new(0, temp_dir, WAL_MAX_SIZE_BYTES);
+        let mut wal = Wal::new(0, temp_dir.path().to_path_buf(), WAL_MAX_SEGMENT_SIZE_BYTES);
 
         let entry = WalEntry::Put {
             key: b"foo".to_vec(),
             value: b"bar".to_vec(),
         };
-        let wrote = wal.append(entry);
+        let wrote = wal.append(vec![entry]);
         assert_eq!(wal.current_size, wrote);
 
         let file = std::fs::File::open(wal.path()).unwrap();
-        let wal: WalEntry = bincode::deserialize_from(file).unwrap();
-        match wal {
+        let entry: WalEntry = bincode::deserialize_from(&file).unwrap();
+        match entry {
             WalEntry::Put { key, value } => {
                 assert_eq!(&String::from_utf8_lossy(&key), "foo");
                 assert_eq!(&String::from_utf8_lossy(&value), "bar");
             }
             WalEntry::Delete { key: _ } => panic!("Expected put operation, got delete!"),
         }
-    }
 
-    #[test]
-    fn next_id() {
-        let temp_dir = TempDir::new("write_wal").unwrap();
-        let wal = Wal::new(0, temp_dir, WAL_MAX_SIZE_BYTES);
-        assert_eq!(wal.next_id(), 1);
-        assert_eq!(wal.next_id(), 2);
+        // Write multiple entries
+        let mut entries = vec![];
+        for _ in 0..10 {
+            entries.push(WalEntry::Put {
+                key: b"foo".to_vec(),
+                value: b"bar".to_vec(),
+            });
+        }
+        let mut wal = Wal::new(1, temp_dir.path().to_path_buf(), WAL_MAX_SEGMENT_SIZE_BYTES);
+        let wrote = wal.append(entries);
+        assert_eq!(wal.current_size, wrote);
+        let wal_file = BufReader::new(std::fs::File::open(wal.path()).unwrap());
+        for line in wal_file.lines() {
+            let entry: WalEntry = bincode::deserialize(line.unwrap().as_bytes()).unwrap();
+            match entry {
+                WalEntry::Put { key, value } => {
+                    assert_eq!(&String::from_utf8_lossy(&key), "foo");
+                    assert_eq!(&String::from_utf8_lossy(&value), "bar");
+                }
+                WalEntry::Delete { key: _ } => panic!("Expected put operation, got delete!"),
+            }
+        }
     }
 
     #[test]
     fn id() {
         let temp_dir = TempDir::new("write_wal").unwrap();
-        let wal = Wal::new(0, temp_dir, WAL_MAX_SIZE_BYTES);
-        assert_eq!(wal.id(), 0);
-        assert_eq!(wal.next_id(), 1);
+        let wal = Wal::new(0, temp_dir.into_path(), WAL_MAX_SEGMENT_SIZE_BYTES);
+        assert_eq!(wal.segment.id(), 0);
     }
 
     #[test]
     fn wal_path() {
         let temp_dir = TempDir::new("write_wal").unwrap();
-        let wal = Wal::new(0, &temp_dir, WAL_MAX_SIZE_BYTES);
+        let wal = Wal::new(0, temp_dir.path().to_path_buf(), WAL_MAX_SEGMENT_SIZE_BYTES);
         assert_eq!(
             wal.path(),
-            temp_dir.path().join("0.wal"),
+            temp_dir.into_path().join("0.wal"),
             "WAL filename was not in the expected format"
         );
     }
@@ -170,20 +201,23 @@ mod test {
     #[test]
     fn rotation() {
         let temp_dir = TempDir::new("write_wal").unwrap();
-        let mut wal = Wal::new(0, temp_dir, TINY_WAL_MAX_SIZE); // Force rotation quickly
+        let mut wal = Wal::new(0, temp_dir.into_path(), WAL_MAX_SEGMENT_SIZE_BYTES);
 
         for _ in 0..3 {
-            wal.append(WalEntry::Put {
+            wal.append(vec![WalEntry::Put {
                 key: b"foo".to_vec(),
                 value: b"bar".to_vec(),
-            });
+            }]);
         }
+
+        assert_eq!(wal.closed_segments.len(), 0, "No closed segments");
+        assert_eq!(wal.segment.id(), 0);
+
+        wal.rotate();
+
         assert_eq!(wal.current_size, 0, "WAL size should reset");
-        assert_ne!(
-            wal.id.load(Ordering::Acquire),
-            0,
-            "WAL rotation has not occurred"
-        );
-        assert_eq!(wal.id.load(Ordering::Acquire), 3);
+        assert_ne!(wal.segment.id(), 0, "WAL rotation has not occurred");
+        assert_eq!(wal.closed_segments.len(), 1);
+        assert_eq!(wal.segment.id(), 1);
     }
 }
