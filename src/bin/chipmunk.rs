138b3b12ecf7f80a235404ebd3d0d294a21edca2
Jack Dockerty <jdockerty19@gmail.com>
2024-08-18 22:24:28 +0100

    feat: use tcp server in chipmunk.rs

--- src/bin/chipmunk.rs
+++ src/bin/chipmunk.rs
@@ -1,3 +1,24 @@
+use std::sync::mpsc::channel;
+
+use chipmunk::{
+    config::{ChipmunkConfig, MemtableConfig, WalConfig},
+    server::Chipmunk,
+};
+
 fn main() {
-    todo!() // Basic server for k-v operations here
+    let config = ChipmunkConfig {
+        addr: "127.0.0.1:5000".to_string(),
+        wal: WalConfig {
+            id: 0,
+            max_size: 1024,
+            log_directory: "./".into(),
+        },
+        memtable: MemtableConfig {
+            id: 0,
+            max_size: 1024,
+        },
+    };
+    let mut h = Chipmunk::new(config);
+    eprintln!("Listening on tcp://127.0.0.1:5000");
+    h.run();
 }
