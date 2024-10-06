0f63694470304a0eaa14b52c9947c5d458fe6fbd
Jack Dockerty <jdockerty19@gmail.com>
2024-10-06 18:12:12 +0100

    feat: add chipmunkerror

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,20 @@
+use std::io;
+
 pub mod config;
 pub mod server;
 
 mod lsm;
 mod memtable;
 mod wal;
+
+#[derive(Debug, thiserror::Error)]
+pub enum ChipmunkError {
+    #[error("An invalid segment file")]
+    InvalidSegment { name: String, source: io::Error },
+
+    #[error("Unable to fsync the current segment")]
+    SegmentFsync(io::Error),
+
+    #[error("Could not append to the WAL segment")]
+    WalAppend(io::Error)
+}
