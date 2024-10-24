949ff1549e25bb30096adec875e1f72d9337e9a1
Jack Dockerty <jdockerty19@gmail.com>
2024-10-24 21:55:12 +0100

    feat: populate crud scaffolding

--- src/client.rs
+++ src/client.rs
@@ -33,6 +33,7 @@ pub enum ClientError {
 /// Interact with a remote chipmunk store.
 pub struct ChipmunkClient {
     host: SocketAddr,
+    client: reqwest::Client,
 }
 
 impl ChipmunkClient {
@@ -42,30 +43,75 @@ impl ChipmunkClient {
             host: host
                 .parse()
                 .map_err(|e| ClientError::InvalidHost { host, source: e })?,
+            client: reqwest::Client::new(),
         })
     }
 
     /// Check the remote server is available to accept connections, returning
     /// [`Some`] if available and [`None`] otherwise.
     pub async fn ping(&self) -> Option<()> {
-        tokio::net::TcpStream::connect(self.host)
+        self.client
+            .get(format!("http://{}/health", self.host))
+            .send()
             .await
             .ok()
             .map(|_| ())
     }
 
     /// Get a value from the remote store, addressed by its key.
-    pub async fn get(&self, _key: String) {
-        todo!()
+    pub async fn get(&self, key: &str) -> Result<Option<String>, ClientError> {
+        let resp = self
+            .client
+            .get(format!("http://{}/api/v1/{}", self.host, key))
+            .send()
+            .await
+            .map_err(|e| ClientError::GetOp {
+                key_name: key.to_string(),
+                source: e,
+            })?;
+
+        if resp.status() == StatusCode::NOT_FOUND {
+            return Ok(None);
+        }
+
+        let body = resp.bytes().await.map_err(|e| ClientError::GetOp {
+            key_name: key.to_string(),
+            source: e,
+        })?;
+        Ok(Some(String::from_utf8_lossy(&body).to_string()))
     }
 
     /// Insert a new key-value pair.
-    pub async fn insert(&self, _key: String, _value: Vec<u8>) {
-        todo!()
+    pub async fn insert(&self, key: &str, value: &str) -> Result<(), ClientError> {
+        let req = self
+            .client
+            .post(format!("http://{}/api/v1", self.host))
+            .body(format!("{key}={value}"));
+
+        let resp = req.send().await.map_err(|e| ClientError::InsertOp {
+            key_name: key.to_string(),
+            source: e,
+        })?;
+
+        match resp.error_for_status() {
+            Ok(_) => Ok(()),
+            Err(e) => Err(ClientError::InsertOp {
+                key_name: key.to_string(),
+                source: e,
+            }),
+        }
     }
 
     /// Delete a key from the remote store.
-    pub async fn delete(&self, _key: String) {
-        todo!()
+    pub async fn delete(&self, key: &str) -> Result<(), ClientError> {
+        self.client
+            .delete(format!("http://{}/api/v1/{}", self.host, key))
+            .send()
+            .await
+            .map(|_| Ok(()))
+            .map_err(|e| ClientError::DeleteOp {
+                key_name: key.to_string(),
+                source: e,
+            })?
     }
 }
