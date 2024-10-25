ee91ca08ef1232b8e96f870270c2407f30901211
Jack <56563911+jdockerty@users.noreply.github.com>
2024-10-25 14:00:13 +0100

    feat: add chipmunk client (#34)
    * feat: add basic client scaffold
    
    * chore: remove unused err
    
    * docs: expand docstrings
    
    * feat: use reqwest::Error wraps
    
    * feat: populate crud scaffolding
    
    * feat: add basic client binary
    
    * feat: use clap for client cli
    
    * feat: add healthcheck cmd

---
+++ src/bin/client.rs
@@ -0,0 +1,47 @@
+use chipmunk::client::ChipmunkClient;
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Clone, Parser)]
+struct Cli {
+    /// Host address of the remote chipmunk store.
+    #[arg(long, default_value = "127.0.0.1:5000")]
+    host: String,
+
+    #[clap(subcommand)]
+    commands: Commands,
+}
+
+#[derive(Debug, Clone, Subcommand)]
+enum Commands {
+    /// Perform a health check against the store.
+    Health,
+    /// Get a value from the store, addressed by key.
+    Get { key: String },
+    /// Insert a key-value pair to the store.
+    Insert { key: String, value: String },
+    /// Delete a pre-existing key-value pair from the store, addressed by key.
+    Delete { key: String },
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
+    let cli = Cli::parse();
+
+    let client = ChipmunkClient::try_new(cli.host.clone())?;
+
+    match cli.commands {
+        Commands::Get { key } => {
+            match client.get(&key).await? {
+                Some(value) => println!("{value}"),
+                None => println!("'{key}' does not exist"),
+            };
+        }
+        Commands::Insert { key, value } => client.insert(&key, &value).await?,
+        Commands::Delete { key } => client.delete(&key).await?,
+        Commands::Health => match client.ping().await {
+            Some(_) => println!("{} is healthy", cli.host),
+            None => println!("{} is unhealthy", cli.host),
+        },
+    }
+    Ok(())
+}
